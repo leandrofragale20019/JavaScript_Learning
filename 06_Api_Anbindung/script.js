bba472ce4fc67b1ec2655f0908d754e5c056fe5e
Fragale Leandro <frle6@pstmac320.local>
2026-03-31 16:13:41 +0200

    Project 06 improvement

--- 06_Api_Anbindung/script.js
+++ 06_Api_Anbindung/script.js
@@ -1,9 +1,13 @@
-const url = "https://pokeapi.co/api/v2/pokemon"
+
+const baseurl = "https://pokeapi.co/api/v2/pokemon/"
+/** @type {HTMLInputElement} */
+const input = document.getElementById("input")
+const searchbtn = document.getElementById("searchbtn")
 const content = document.getElementById("content")
 
-addEventListener("DOMContentLoaded", load)
+//addEventListener("DOMContentLoaded", load)
 
-function load() {
+/*function load() {
     fetch(url) 
     .then(r => r.json())
     .then(data => {
@@ -15,3 +19,22 @@ function load() {
         });
     })
 }
+*/
+
+searchbtn.onclick = function() {
+    const name = input.value.toLocaleLowerCase();
+
+    const finalurl = baseurl + name;
+
+    fetch(finalurl)
+    .then(r => r.json())
+    .then(data => {
+        content.innerHTML = "";
+        
+            content.innerHTML = 
+            `<h3>${data.name}</h3>
+            <img src="${data.sprites.front_default}">
+            <p>${data.game_index}</p>
+        `;
+        })
+    }
